28858e26a6c5b3d808d6272f588638dd6e50d310
Thiago Romão <thiaroll@gmail.com>
2019-02-05 14:10:37 -0200

    condições e tipos de dados

---
+++ Java Script/condições.js
@@ -0,0 +1,35 @@
+// if (bool) { }
+
+//var teste = true;
+//var teste = x == 15;
+var x = 17;
+var y;
+
+if (x > 18)
+{
+  y = "maior";
+}
+else if (x == 18)
+{
+  y = "igual";
+}
+else
+{
+  y = "menor";
+}
+
+console.log(y);
+
+switch(x)
+{
+  case 18:
+   y = 'dezoito';
+   break;
+  case 19:
+   y = 'dezenove';
+   break;
+  default:
+   y = "sei lá" ;
+}
+
+console.log(y);
---
+++ Java Script/tipos-de-dados.js
@@ -0,0 +1,30 @@
+// números
+10;
+10.5
+
+// strings
+"Olá";
+'Olá;'
+
+// booleanos
+true;
+false;
+
+// arrays
+
+[10, "oi",false];
+
+// objetos
+
+{ nome: 'João', 'das Neves' };
+
+// TIpagem Dinâmica
+
+var y = 15;
+y = "olá";
+
+// Outros valores
+
+var z; //undefined
+var pessoa = null;
+
