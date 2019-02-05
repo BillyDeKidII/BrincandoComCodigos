5ccab78893713ba2be99223aeff4e429c9583397
Thiago Romão <thiaroll@gmail.com>
2019-02-05 10:49:19 -0200

    começando a brincar com java script

---
+++ Java Script/sintaxe-basica.js
@@ -0,0 +1,40 @@
+// statementes terminam com ponto e virgula
+var x = 15; 
+var y = 12;
+
+// Valores : literais e variáveis
+
+// Literais
+
+10;
+10.5;
+"Olá mundo";
+'Olá mundo';
+
+// Variáveis
+
+var x;
+
+// Operadores
+
+x = 10;
+10 + 5;
+20 - 10;
+40 * 5;
+20 / 10;
+
+// Expressões
+
+y + 10;
+20 * 30;
+5 * y;
+
+/*
+bloco de comentarios
+*/
+
+// Identificadores
+
+var sintaxeBasica;
+var _sintaxeBasica;
+var $sintaxeBasica;
