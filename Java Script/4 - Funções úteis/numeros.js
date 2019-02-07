a5bc7dcc7c5e6a3384e563082b90b89a22b97aa5
Thiago Romão <thiaroll@gmail.com>
2019-02-07 14:29:26 -0200

    Organização de pastas e finalização dos modulos 5 e 6 juntamente com o final do curso de Java Script

---
+++ Java Script/4 - Funções úteis/numeros.js
@@ -0,0 +1,14 @@
+var x = 456e5;  //45600000
+var y = 123e-6; //0.000123
+var z = 0.99999999999999999;
+var a = 0xfff;
+var b = 07777;
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b.toString(2));
+console.log(x.toExponential());
+console.log((3.357).toFixed(2));
+console.log((3.245).toPrecision(2));
