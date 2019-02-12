f127fa6c5a3b86c2bd5f4b8e6034238613831d80
Thiago Romão <thiaroll@gmail.com>
2019-02-12 17:22:04 -0200

    começando ionic4 lendo o doc inteiro

--- Ionic/tutorial/src/app/app.module.ts
+++ Ionic/tutorial/src/app/app.module.ts
@@ -8,12 +8,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FeedPageModule } from './feed/feed.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FeedPageModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
