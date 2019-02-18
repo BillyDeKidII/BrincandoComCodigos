9c1d168678a3aa50db46d3cb16c5229ceb09f76d
Thiago Romão <thiaroll@gmail.com>
2019-02-18 18:08:57 -0300

    VAI SE FUDER DEU ERRADO

--- Spring MVC/cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ Spring MVC/cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,14 +1,25 @@
 package com.algaworks.cobranca.controller;
 
+import com.algaworks.cobranca.model.Titulo;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 	
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo()
 	{
+		return "CadastroTitulo";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo Titulo)
+	{
+		System.out.println(">>> " + Titulo.getDescricao());
 		return "CadastroTitulo";
 	}
 
