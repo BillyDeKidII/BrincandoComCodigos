696a0e750dbd284b675bd088e5c213cc8e6383ab
Thiago Romão <thiaroll@gmail.com>
2019-02-25 23:50:25 -0300

    correção de bugs

--- Python/Desafios/Desafio06.py
+++ Python/Desafios/Desafio06.py
@@ -2,4 +2,4 @@ num = int(input('Insira um número : '))
 dobro = num * 2
 triplo = num * 3
 raiz = num**(1/2)
-print('O dobro é {}\no triplo é {}\ne a raiz é {:.2f}'.format(dobro, triplo, raiz))
+print('O dobro é {}\n o triplo é {}\n e a raiz é {:.2f}'.format(dobro, triplo, raiz))
