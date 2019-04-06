a5b141daf9fb5e8e1cd8daa3339e94aecca73ce6
Thiago Romão <thiaroll@gmail.com>
2019-04-06 15:24:48 -0300

    Feito alteração com sucesso, adicionado o 'desenho da tela' e rodou perfeitamente

--- Python/Jogo/slidepuzzle/mainGame.py
+++ Python/Jogo/slidepuzzle/mainGame.py
@@ -1,5 +1,17 @@
-import pygame, sys, os
+import os
+import pygame
+import sys
 
+
+class SlidePuzzle:
+    def __init__(self,gs,ts,ms):
+        self.gs,self.ts,self.ms = gs,ts,ms
+
+    def update (self,dt):
+        pass
+
+    def draw(self,screen):
+        pass
 
 def main():
     pygame.init()
@@ -7,17 +19,21 @@ def main():
     pygame.display.set_caption('Slide Puzzle')
     screen = pygame.display.set_mode((800, 600))
     fpsclock = pygame.time.Clock()
+    program = SlidePuzzle((3,3),100,5)
 
     while True:
+        dt = fpsclock.tick()/1000
 
         screen.fill((0, 0, 0))
+        program.draw(screen)
         pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit();
                 sys.exit()
+        
+        program.update(dt)
 
-#THIAGO
 if __name__ == '__main__':
     main()
