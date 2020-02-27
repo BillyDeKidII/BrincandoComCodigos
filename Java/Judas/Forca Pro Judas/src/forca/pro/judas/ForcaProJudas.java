36f078ebe81a3a2536e92bbf0418546fe042a2e1
Thiago Romão <thiaroll@gmail.com>
2020-02-26 22:44:44 -0300

    upando está merda

--- Java/Judas/Forca Pro Judas/src/forca/pro/judas/ForcaProJudas.java
+++ Java/Judas/Forca Pro Judas/src/forca/pro/judas/ForcaProJudas.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 
 public class ForcaProJudas {
 
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        String secreta = "microfone";
-        char[] copia = new char [ secreta.length()];
-        System.out.println(secreta);
-        for (int i = 0; i < secreta.length(); i++)
-        {
-            //System.out.print("_ ");
-            copia[i] = '_';
-            System.out.print(copia[i] + " ");
-        }
-        do{
+    public static void main(final String[] args) {
+      final Scanner keyboard = new Scanner(System.in);
+      final String secreta = "microfone";
+      final char[] copia = new char[secreta.length()];
+      System.out.println(secreta);
+      for (int i = 0; i < secreta.length(); i++) {
+        // System.out.print("_ ");
+        copia[i] = '_';
+        System.out.print(copia[i] + " ");
+      }
+      do {
         System.out.println("");
         System.out.println(" Insira a letra ");
-        char tentativa = keyboard.next().charAt(0);
+        final char tentativa = keyboard.next().charAt(0);
         for (int i = 0; i < secreta.length(); i++)
         {
           if(tentativa == secreta.charAt(i))
